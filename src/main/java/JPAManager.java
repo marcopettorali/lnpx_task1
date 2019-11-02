d5384ec32c43a3c753f5c928ef844f4230afa077
Marco <Marco@DESKTOP-5QVK6PN>
2019-11-02 17:45:04 +0100

    moved jpa classes to default package

---
+++ src/main/java/JPAManager.java
@@ -0,0 +1,35 @@
+
+
+
+import java.util.*;
+import javax.persistence.*;
+
+
+public class JPAManager {
+    private static final String queryAvailableRooms = ""
+            + "select D.RoomName,D.Capacity - D.Booked as Available, D.Capacity, D.RowNumber	"
+            + "from (   "
+            + "          select b.RoomName, count(*) as Booked ,r.Capacity, r.RowNumber"
+            + "          from booking b inner join room r on (r.RoomName=b.RoomName)"
+            + "	    where b.StartTime=? and b.Date=?	"
+            + "	    group by b.RoomName "
+            + "		) as D "
+            + "where D.Capacity - D.Booked > 0 "
+            + " union  select  r1.RoomName, r1.Capacity,r1.Capacity,r1.RowNumber "
+            + "   from room r1 "
+            + "   where r1.roomName not in (select b1.roomName"
+            + "			         from booking b1"
+            + "                             where b1.StartTime=? and b1.Date=?);";
+    
+    private static final EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("lnpx_lnpx_task1_jar_1.0-SNAPSHOTPU");
+    private static final EntityManager emManager = emFactory.createEntityManager();
+    public static void createPC(PC newPC){
+        emManager.persist(newPC);
+        emManager.getTransaction().commit();
+    } 
+    
+    public static List<Room> loadRooms(String date, String time){
+        TypedQuery<Room> = emManager.createNativeQuery(JPAManager.queryAvailableRooms, Room.class);
+        
+    }
+}
